b898c949e9a760f659c9aa330ac9fdaad2bdda81
hktari <bkamnik1995@gmail.com>
2022-08-03 23:30:04 +0200

    move browser automation to seperate file

--- main.js
+++ main.js
@@ -1,53 +1,14 @@
-const puppeteer = require('puppeteer');
+const assistantApp = require('./assistant-app')
 
+let credentials = { username: 'bostjankamnik45', password: `zxA/)#]*'0.\`r:D;OxQH` }
 
-function delay(waitTime) {
-    return new Promise((resolve) => {
-        setTimeout(() => {
-            resolve(true);
-        }, waitTime);
-    });
-
-}
-
-
-(async () => {
-    const browser = await puppeteer.launch({
-        headless: false,
-        slowMo: 50, // slow down by 250ms
-        // devtools: true
-    }); // default is true
-
+async function run() {
     try {
-        const page = await browser.newPage();
-        await page.goto('https://mddsz.si/oa/oa_web_ws/r/osebna_asistenca');
-        await page.waitForNavigation(); // The promise resolves after navigation has finished
-        let credentials = { username: 'bostjankamnik45', password: `zxA/)#]*'0.\`r:D;OxQH` }
-
-        const usernameInput = await page.$('#P9999_USERNAME')
-        await usernameInput.type(credentials.username)
-
-        const pwdInput = await page.$('#P9999_PASSWORD');
-        await pwdInput.type(credentials.password);
-
-        const loginBtn = await page.$('.t-Login-buttons button')
-        loginBtn.click()
-
-        const startBtnSelector = 'button.t-Button--success';
-        const stopBtnSelector = 'button.t-Button--danger';
-        // make sure start button is enabled
-
-        const startBtn = await page.waitForSelector(startBtnSelector) // wait 30 sec for login
-        await delay(3000);
-
-        console.log('start btn disabled:', await page.$eval(startBtnSelector, btn => btn.disabled))
-        console.log('stop btn disabled:', await page.$eval(stopBtnSelector, btn => btn.disabled))
-        // await startBtn.click();
-
-        // await page.screenshot({ path: 'example.png' });
-
-        // await browser.close();
+        await assistantApp.addEntry(credentials);
+        console.log('success');
     } catch (error) {
         console.error(error);
     }
-})();
+}
+
+run()
