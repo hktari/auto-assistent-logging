134e757512b2d2541a60c01b10660dd327449a9a
hktari <bkamnik1995@gmail.com>
2022-08-04 10:51:52 +0200

    add button disabled logic

--- mock-assistant-app/src/pages/Dashboard.jsx
+++ mock-assistant-app/src/pages/Dashboard.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
+    const [startBtnDisabled, setStartBtnDisabled] = useState(false)
+    const [stopBtnDisabled, setStopBtnDisabled] = useState(true)
+
+    function onStart(ev) {
+        console.log('START CLICKED')
+        setStartBtnDisabled(!startBtnDisabled);
+        setStopBtnDisabled(!stopBtnDisabled);
+    }
+
+    function onStop(ev) {
+        console.log('STOP CLICKED')
+        setStartBtnDisabled(!startBtnDisabled);
+        setStopBtnDisabled(!stopBtnDisabled);
+    }
+
+
+    return (
+        <div>
+            <button disabled={startBtnDisabled} className='t-Button--success' onClick={onStart}>START</button>
+            <button disabled={stopBtnDisabled} className='t-Button--danger' onClick={onStop}>STOP</button>
+        </div>
+    )
 }
 
 export default Dashboard
