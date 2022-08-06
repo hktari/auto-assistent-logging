a0df19d4951dac7478cd6be1170fa7ad84ed2566
hktari <bkamnik1995@gmail.com>
2022-08-07 01:06:20 +0200

    add workday DELETE

--- auto-assistant-logging/backend/routers/workdayConfigRouter.js
+++ auto-assistant-logging/backend/routers/workdayConfigRouter.js
@@ -8,12 +8,27 @@ const router = express.Router()
 router.param('id', loadLoginInfoID);
 
 router.route('/account/:id/workday')
+    .get((req, res, next) => {
+        db.query(`SELECT dc.id, date, start_at, end_at, automation_type
+                    FROM daily_config dc JOIN login_info li ON dc.login_info_id = li.id 
+                    WHERE li.id = $1`, [req.loginInfoID])
+            .then(result => res.status(200).json(result.rows))
+            .catch(err => next(err))
+    })
     .post((req, res, next) => {
         log(debug(req.loginInfoID))
         db.query(`INSERT INTO daily_config (login_info_id, date, start_at, end_at, automation_type)
                     VALUES ($1, $2, $3, $4, $5)
                     RETURNING *`, [req.loginInfoID, req.body.date, req.body.start_at, req.body.end_at, req.body.automation_type])
             .then(result => res.status(result.rowCount > 0 ? 200 : 400).json(result.rows))
+            .catch(err => next(err))
+    })
+
+router.route('/account/:id/workday/:workdayId')
+    .delete((req, res, next) => {
+        db.query(`DELETE FROM daily_config
+                            WHERE id = $1`, [req.params.workdayId])
+            .then(result => res.sendStatus(result.rowCount > 0 ? 200 : 404))
             .catch(err => next(err))
     })
 
