4820b0d7fa9917b26e14b8b8dbcf2eb9eaaf54e5
hktari <bkamnik1995@gmail.com>
2022-08-06 22:51:29 +0200

    add more account handlers

--- auto-assistant-logging/backend/routers/accountRouter.js
+++ auto-assistant-logging/backend/routers/accountRouter.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const chalk = require('chalk')
 const { db } = require('../services/database');
-const { log, error } = require('../util/logging');
+const { log, error, info } = require('../util/logging');
 
 const router = express.Router();
 
@@ -31,20 +31,58 @@ router.route('/account/')
         }
     })
 
-router.route('/account/:id')
-    .get((req, res) => {
-        console.log(chalk.gray('[GET] /account/' + req.params.id))
-        res.status(200).send('not implemented')
-    })
-    .delete((req, res) => {
-        console.log(chalk.gray('[DELETE] /account/' + req.params.id))
-        res.status(200).send('not implemented')
-    })
-    .put((req, res) => {
-        console.log(chalk.gray('[PUT] /account/' + req.params.id))
-        res.status(200).send('not implemented')
 
+router.route('/account/:id')
+    .get(async (req, res, next) => {
+        console.log(chalk.gray('[GET] /account/' + req.params.id))
+        try {
+            const queryResult = await db.query(`SELECT email, "automationEnabled"
+                                                FROM account WHERE id = $1`, [req.params.id])
+            if (queryResult.rowCount > 0) {
+                res.status(200).json(queryResult.rows)
+            } else {
+                res.sendStatus(404);
+            }
+        } catch (e) {
+            next(e)
+        }
     })
+    .delete(async (req, res, next) => {
+        console.log(chalk.gray('[DELETE] /account/' + req.params.id))
+
+        try {
+            const queryResult = await db.query('DELETE FROM ACCOUNT WHERE id = $1', [req.params.id])
+            res.sendStatus(queryResult.rowCount > 0 ? 200 : 404);
+        } catch (e) {
+            next(e)
+        }
+    })
+    .put(async (req, res, next) => {
+        console.log(chalk.gray('[PUT] /account/' + req.params.id))
+        try {
+            const queryResult = await db.query(`UPDATE ACCOUNT SET "automationEnabled" = $2
+                                            WHERE id = $1`, [req.params.id, req.body.automationEnabled])
+            if (queryResult.rowCount > 0) {
+                res.sendStatus(200)
+            } else {
+                log(info('Failed to update account with id: ' + req.params.id))
+                res.sendStatus(404);
+            }
+        } catch (e) {
+            next(e)
+        }
+    })
+
+router.post('/account/:id/reset-password', async (req, res, next) => {
+    log(info(`[POST] ${req.params.id}/reset-password`))
+    try {
+        const queryResult = await db.query(`UPDATE ACCOUNT SET password = $1
+                                                WHERE id = $2`, [req.body.password, req.params.id])
+        res.sendStatus(queryResult.rowCount > 0 ? 200 : 404);
+    } catch (e) {
+        next(e)
+    }
+})
 
 
 module.exports = router;
