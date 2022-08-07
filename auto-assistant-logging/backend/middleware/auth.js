bdd5d392180a5d3ec0c7de04a6c92a0ef688939f
hktari <bkamnik1995@gmail.com>
2022-08-07 12:46:24 +0200

    fix 'createToken()' and exclude password from 'login' api

--- auto-assistant-logging/backend/middleware/auth.js
+++ auto-assistant-logging/backend/middleware/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 
 
 function createToken(email, admin = false) {
-    jwt.sign({
+    const token = jwt.sign({
         exp: Math.floor(Date.now() / 1000) + (60 * 60 * process.env.JWT_EXPIRES_IN_HRS),
         data: { email: email, admin: admin }
     }, process.env.JWT_SECRET);
--- auto-assistant-logging/backend/routers/authRouter.js
+++ auto-assistant-logging/backend/routers/authRouter.js
@@ -6,20 +6,21 @@ const { log, info } = require('../util/logging')
 const { route } = require('./accountRouter')
 const router = express.Router()
 
-router.post('/login', (req, res, next) => {
-    const queryResult = db.query(`SELECT id, email, password
+router.post('/login', async (req, res, next) => {
+    const queryResult = await db.query(`SELECT id, email, password, "automationEnabled"
                                 FROM ACCOUNT
                                 WHERE email=$1`, [req.body.email])
     if (queryResult.rowCount === 0) {
         res.sendStatus(400);
     } else {
         const account = queryResult.rows[0]
-        if (encrypt.compare(req.body.password, account.password)) {
+        if (await encrypt.compare(req.body.password, account.password)) {
             log(info('login succeeded: ' + req.body.email))
 
             res.status(200).json({
                 token: createToken(req.body.email),
-                ...account
+                email: account.email,
+                automationEnabled: account.automationEnabled
             })
         } else {
             log(info('credentials mismatch'))
