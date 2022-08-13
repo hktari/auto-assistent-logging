0508193462b9a5cb356611f059f7d458510b826e
hktari <bkamnik1995@gmail.com>
2022-08-13 17:36:04 +0200

    remove quotation marks from docker compose file env vars

--- browser-automation/main.js
+++ browser-automation/main.js
@@ -10,7 +10,7 @@ const bree = new Bree({
             // runs `./jobs/email.js` every minute
             name: 'auto-assistant',
             // interval: '5s',
-            cron: '* * * * 5 *',
+            cron: '* * * 5 *',
             // run on start as well
             timeout: 0
         }
