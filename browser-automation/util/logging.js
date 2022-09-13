0e347e16525bd6d120612969bcd6eae2a59a91ea
hktari <bkamnik1995@gmail.com>
2022-09-13 21:37:10 +0200

    add error log file to winston config

--- browser-automation/util/logging.js
+++ browser-automation/util/logging.js
@@ -1,4 +1,5 @@
 const winston = require('winston')
+const dailyRotate = require("winston-daily-rotate-file");
 
 const { format } = require('logform');
 
@@ -10,31 +11,38 @@ const alignedWithColorsAndTime = format.combine(
 );
 
 const options = {
-    file: {
-        level: 'info',
-        filename: './logs/app.log',
-        handleExceptions: true,
-        json: true,
-        maxsize: 5242880, // 5MB
-        maxFiles: 5,
-        colorize: false,
-    },
-    console: {
+    errFile: new winston.transports.DailyRotateFile({
+        filename: 'logs/error-%DATE%.log',
+        datePattern: 'YYYY-MM-DD-HH',
+        level: 'error',
+
+    }),
+    logFile: new winston.transports.DailyRotateFile({
+        filename: 'logs/all-%DATE%.log',
+        datePattern: 'YYYY-MM-DD-HH',
+        level: 'debug'
+    }),
+    console: new winston.transports.Console({
         level: 'debug',
         handleExceptions: true,
         json: false,
         colorize: true,
         format: alignedWithColorsAndTime
-    },
+    }),
 };
 
 const logger = winston.createLogger({
-    levels: winston.config.npm.levels,
+    level: 'info',
     transports: [
-        // new winston.transports.File(options.file),
-        new winston.transports.Console(options.console)
+        options.logFile,
+        options.errFile,
     ],
     exitOnError: false
 })
+
+// If we're not in production then log to the `console`  
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(options.console);
+}
 
 module.exports = logger
