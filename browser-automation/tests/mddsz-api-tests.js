1de7324e6f1c7884e90a904de93d8472079ed5c7
hktari <bkamnik1995@gmail.com>
2022-09-13 22:57:40 +0200

    add test

--- browser-automation/tests/mddsz-api-tests.js
+++ browser-automation/tests/mddsz-api-tests.js
@@ -1,15 +1,29 @@
 const { expect } = require('chai')
 const { describe, it } = require('mocha');
-const { AUTOMATE_ACTION } = require('../interface');
+const { AUTOMATE_ACTION, LogEntry, LOG_ENTRY_STATUS } = require('../interface');
 const { executeAction, MDDSZApiError } = require('../mddsz-api')
+const db = require('../dbFacade')
+
 // require('chai-as-promised')
 
 describe('mddsz-api', function () {
     this.timeout(60000);
-    
+
     it('executeAction', (done) => {
         executeAction(process.env.MDDSZ_USERNAME, process.env.MDDSZ_PASSWORD, AUTOMATE_ACTION.START_BTN)
             .then(res => done())
+            .catch(err => done(err))
+    })
+
+    it('executeAction adds log entry', (done) => {
+        executeAction(process.env.MDDSZ_USERNAME, process.env.MDDSZ_PASSWORD, AUTOMATE_ACTION.START_BTN)
+            .then(res => {
+                db.getLogEntries(process.env.MDDSZ_USERNAME, new Date()).then(queryResult => {
+                    expect(queryResult.rowCount).to.be.greaterThan(0)
+                    expect(queryResult.rows)
+                })
+                done()
+            })
             .catch(err => done(err))
     })
 })
