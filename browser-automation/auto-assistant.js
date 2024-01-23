bf6107d3c3ee1fd328e4e46cf9c553d0f41c71f9
hktari <bkamnik1995@gmail.com>
2024-01-23 15:21:05 +0100

    update interface of handleAutomationForUser(). It takes config object

--- browser-automation/auto-assistant.js
+++ browser-automation/auto-assistant.js
@@ -1,149 +1,196 @@
-const { executeAction } = require('./automation/mddsz-api');
-const logger = require('./util/logging')
-const { getActionsForDate, AutomationActionResult } = require('./util/actions');
-const { AUTOMATE_ACTION, WORKDAY_CONFIG_AUTOMATION_TYPE, LOG_ENTRY_STATUS, WorkdayConfig, CONFIG_TYPE } = require('./interface')
+const { executeAction } = require("./automation/mddsz-api");
+const logger = require("./util/logging");
+const { getActionsForDate, AutomationActionResult } = require("./util/actions");
+const {
+  AUTOMATE_ACTION,
+  WORKDAY_CONFIG_AUTOMATION_TYPE,
+  LOG_ENTRY_STATUS,
+  WorkdayConfig,
+  CONFIG_TYPE,
+} = require("./interface");
 
-const db = require('./dbFacade')
+const db = require("./dbFacade");
 
 // Filter out exceptions made for weekly configuration
 function _filterThroughExceptions(actionsList, exceptions) {
-    return actionsList.filter(action => {
-        return action.configType === CONFIG_TYPE.DAILY
-            || (action.configType === CONFIG_TYPE.WEEKLY && !exceptions.some(ex => ex.action === action.actionType))
-    })
+  return actionsList.filter((action) => {
+    return (
+      action.configType === CONFIG_TYPE.DAILY ||
+      (action.configType === CONFIG_TYPE.WEEKLY &&
+        !exceptions.some((ex) => ex.action === action.actionType))
+    );
+  });
 }
 
 function _isSameDay(datetimeFirst, datetimeSecond) {
-    return datetimeFirst.toISOString().substring(0, 10) == datetimeSecond.toISOString().substring(0, 10)
+  return (
+    datetimeFirst.toISOString().substring(0, 10) ==
+    datetimeSecond.toISOString().substring(0, 10)
+  );
 }
 
 function _datetimeRangeCompare(datetimeFirst, datetimeSecond, rangeMs = 60000) {
-    logger.debug(`comparing  ${datetimeFirst.toUTCString()} : ${datetimeSecond.toUTCString()} `)
+  logger.debug(
+    `comparing  ${datetimeFirst.toUTCString()} : ${datetimeSecond.toUTCString()} `
+  );
 
-    return Math.abs(datetimeFirst.getTime() - datetimeSecond.getTime()) <= rangeMs
+  return (
+    Math.abs(datetimeFirst.getTime() - datetimeSecond.getTime()) <= rangeMs
+  );
 }
 
 /**
  * filters the @param actionsList based on whether there is a successful entry inside @param logEntries
- * @param {AutomationAction[]} actionsList 
- * @param {LogEntry[]} logEntries 
- * @returns 
+ * @param {AutomationAction[]} actionsList
+ * @param {LogEntry[]} logEntries
+ * @returns
  */
 function _filterOutAlreadyExecuted(actionsList, logEntries) {
-    return actionsList.filter(action => {
-        const logEntryMatchesAction = logEntries.some(le => {
-            return le.action === action.actionType
-                && action.configType === le.configType
-                && _isSameDay(action.dueAt, le.timestamp)
-                && le.status === LOG_ENTRY_STATUS.SUCCESSFUL
-        })
-        return !logEntryMatchesAction
-    })
+  return actionsList.filter((action) => {
+    const logEntryMatchesAction = logEntries.some((le) => {
+      return (
+        le.action === action.actionType &&
+        action.configType === le.configType &&
+        _isSameDay(action.dueAt, le.timestamp) &&
+        le.status === LOG_ENTRY_STATUS.SUCCESSFUL
+      );
+    });
+    return !logEntryMatchesAction;
+  });
 }
 
-
 async function _getAndFilterActionsForDate(user, datetime) {
-    logger.debug('processing for datetime: ' + datetime.toUTCString())
-    let actionsForDate = await getActionsForDate(user, datetime)
+  logger.debug("processing for datetime: " + datetime.toUTCString());
+  let actionsForDate = await getActionsForDate(user, datetime);
 
-    const workweekExceptions = await db.getWorkweekExceptions(user.username, datetime)
-    logger.debug('filtering out actions with weekly exceptions');
-    actionsForDate = _filterThroughExceptions(actionsForDate, workweekExceptions)
-    logger.debug(actionsForDate.length + ' left')
+  const workweekExceptions = await db.getWorkweekExceptions(
+    user.username,
+    datetime
+  );
+  logger.debug("filtering out actions with weekly exceptions");
+  actionsForDate = _filterThroughExceptions(actionsForDate, workweekExceptions);
+  logger.debug(actionsForDate.length + " left");
 
-    return actionsForDate
+  return actionsForDate;
 }
 
 function _sortByDatetimeAsc(actions) {
-    let tmp = [...actions]
-    return tmp.sort((a, b) => a.dueAt.getTime() - b.dueAt.getTime())
+  let tmp = [...actions];
+  return tmp.sort((a, b) => a.dueAt.getTime() - b.dueAt.getTime());
 }
 
 /**
  * Checks the database for any pending automation actions for the given user and time.
- * @param {User} user the user object
+ *
+ * @typedef AutomationConfiguration
+ * @property {import('./dbFacade').User} user
+ * @property {import('./dbFacade').ERacuniUserConfiguration} eracuniConfig
+ *
+ * @param {AutomationConfiguration} config 
  * @param {Date} datetime the current time
  * @returns {Promise<AutomationActionResult | null>}
  */
-async function handleAutomationForUser(user, datetime) {
-    logger.debug('\n' + '*'.repeat(50))
-    logger.debug('processing user: ' + user.email)
+async function handleAutomationForUser(config, datetime) {
+  const { user, eracuniConfig } = config;
+  logger.debug("\n" + "*".repeat(50));
+  logger.debug("processing user: " + user.email);
 
-    let actionsPlannedToday = await _getAndFilterActionsForDate(user, datetime)
+  let actionsPlannedToday = await _getAndFilterActionsForDate(user, datetime);
 
-    if (datetime.getUTCHours() <= 8) {
-        const yesterday = new Date(datetime)
-        yesterday.setUTCDate(yesterday.getUTCDate() - 1)
-        actionsPlannedToday = actionsPlannedToday.concat(await _getAndFilterActionsForDate(user, yesterday))
+  if (datetime.getUTCHours() <= 8) {
+    const yesterday = new Date(datetime);
+    yesterday.setUTCDate(yesterday.getUTCDate() - 1);
+    actionsPlannedToday = actionsPlannedToday.concat(
+      await _getAndFilterActionsForDate(user, yesterday)
+    );
+  }
+
+  let logEntriesToday = await db.getLogEntries(user.username, datetime);
+  logger.debug("filtering out already executed actions");
+  actionsPlannedToday = _filterOutAlreadyExecuted(
+    actionsPlannedToday,
+    logEntriesToday
+  );
+  logger.debug(actionsPlannedToday.length + " left");
+
+  // sort by datetime
+  actionsPlannedToday = _sortByDatetimeAsc(actionsPlannedToday);
+
+  // take the first action to be executed
+  let actionToExecute;
+  for (const action of actionsPlannedToday) {
+    logger.debug("considering executing " + action + " ...");
+    if (action.timeToExecute(datetime)) {
+      logger.debug("ok");
+      actionToExecute = new Promise((resolve, reject) => {
+        executeAction(user.username, user.password, action.actionType)
+          .then((result) => {
+            resolve(
+              new AutomationActionResult(
+                action.user,
+                action.actionType,
+                action.configType,
+                action.dueAt,
+                result,
+                null
+              )
+            );
+          })
+          .catch((err) => {
+            reject(
+              new AutomationActionResult(
+                action.user,
+                action.actionType,
+                action.configType,
+                action.dueAt,
+                null,
+                err
+              )
+            );
+          });
+
+        // TODO: execute action on eracuni
+      });
+      break;
     }
+  }
 
-
-    let logEntriesToday = await db.getLogEntries(user.username, datetime)
-    logger.debug('filtering out already executed actions');
-    actionsPlannedToday = _filterOutAlreadyExecuted(actionsPlannedToday, logEntriesToday);
-    logger.debug(actionsPlannedToday.length + ' left')
-
-    // sort by datetime
-    actionsPlannedToday = _sortByDatetimeAsc(actionsPlannedToday)
-
-    // take the first action to be executed
-    let actionToExecute;
-    for (const action of actionsPlannedToday) {
-        logger.debug('considering executing ' + action + ' ...')
-        if (action.timeToExecute(datetime)) {
-            logger.debug('ok')
-            actionToExecute = new Promise((resolve, reject) => {
-                executeAction(user.username, user.password, action.actionType)
-                    .then(result => {
-                        resolve(new AutomationActionResult(action.user, action.actionType, action.configType, action.dueAt, result, null))
-                    })
-                    .catch(err => {
-                        reject(new AutomationActionResult(action.user, action.actionType, action.configType, action.dueAt, null, err))
-                    })
-
-                    // TODO: execute action on eracuni
-            })
-            break;
-        }
-    }
-
-    if (actionToExecute) {
-        return await actionToExecute
-    } else {
-        return null
-    }
+  if (actionToExecute) {
+    return await actionToExecute;
+  } else {
+    return null;
+  }
 }
 
 /**
  * Adds entries into the log_entry table
- * @param {AutomationActionResult} automationResult 
+ * @param {AutomationActionResult} automationResult
  */
 async function logAutomationResult(automationResult) {
-    let logEntryStatus = null;
+  let logEntryStatus = null;
 
-    if (!automationResult.error) {
-        logEntryStatus = LOG_ENTRY_STATUS.SUCCESSFUL;
-    } else {
-        logEntryStatus = LOG_ENTRY_STATUS.FAILED;
-    }
+  if (!automationResult.error) {
+    logEntryStatus = LOG_ENTRY_STATUS.SUCCESSFUL;
+  } else {
+    logEntryStatus = LOG_ENTRY_STATUS.FAILED;
+  }
 
-    logger.debug('adding log entry...')
-    return db.addLogEntry(
-        automationResult.user.login_info_id,
-        logEntryStatus,
-        automationResult.dueAt,
-        automationResult.error?.toString(),
-        automationResult.message,
-        automationResult.actionType,
-        automationResult.configType)
+  logger.debug("adding log entry...");
+  return db.addLogEntry(
+    automationResult.user.login_info_id,
+    logEntryStatus,
+    automationResult.dueAt,
+    automationResult.error?.toString(),
+    automationResult.message,
+    automationResult.actionType,
+    automationResult.configType
+  );
 }
-
 
 module.exports = {
-    handleAutomationForUser,
-    logAutomationResult,
-    _sortByDatetimeAsc,
-    _isSameDay,
-    _filterOutAlreadyExecuted
-
-}
+  handleAutomationForUser,
+  logAutomationResult,
+  _sortByDatetimeAsc,
+  _isSameDay,
+  _filterOutAlreadyExecuted,
+};
--- browser-automation/jobs/browser-automation-job.js
+++ browser-automation/jobs/browser-automation-job.js
@@ -1,66 +1,71 @@
-const db = require('../dbFacade')
-const logger = require('../util/logging')
-const { parentPort } = require('worker_threads');
-const { handleAutomationForUser, logAutomationResult } = require('../auto-assistant');
+const db = require("../dbFacade");
+const logger = require("../util/logging");
+const { parentPort } = require("worker_threads");
+const {
+  handleAutomationForUser,
+  logAutomationResult,
+} = require("../auto-assistant");
 
 // store boolean if the job is cancelled
 let isCancelled = false;
 
 // handle cancellation (this is a very simple example)
 if (parentPort) {
-    parentPort.once('message', (message) => {
-        //
-        // TODO: once we can manipulate concurrency option to p-map
-        // we could make it `Number.MAX_VALUE` here to speed cancellation up
-        // <https://github.com/sindresorhus/p-map/issues/28>
-        //
-        if (message === 'cancel') isCancelled = true;
-    });
+  parentPort.once("message", (message) => {
+    //
+    // TODO: once we can manipulate concurrency option to p-map
+    // we could make it `Number.MAX_VALUE` here to speed cancellation up
+    // <https://github.com/sindresorhus/p-map/issues/28>
+    //
+    if (message === "cancel") isCancelled = true;
+  });
 }
 
-
 (async () => {
-    let jobError = null;
-    try {
-        logger.info(`${'-'.repeat(50)}`)
-        logger.info('start')
+  let jobError = null;
+  try {
+    logger.info(`${"-".repeat(50)}`);
+    logger.info("start");
 
-        const usersToAutomate = await db.getUsers();
-        logger.info(`got ${usersToAutomate.length} users`)
+    const curTime = new Date();
+    logger.info("time: " + curTime.toUTCString());
 
-        const curTime = new Date();
-        logger.info("time: " + curTime.toUTCString())
+    const usersToAutomate = await db.getUsers();
+    logger.info(`got ${usersToAutomate.length} users`);
 
+    let automationResults = [];
+    for (const user of usersToAutomate) {
+      logger.info("fetching eracuni configuration...");
+      const eracuniConfig = await db.getEracuniConfigurationBy(user.accountId);
 
-        let automationResults = []
-        for (const user of usersToAutomate) {
-            // TODO: if exception is thrown inside 'handleAutomationForUser' it will not be logged ?
-            const autoActionForUser = await handleAutomationForUser(user, curTime)
-            if (autoActionForUser === null) {
-                logger.info(`User ${user.username}. Nothing to do...`)
-            } else {
-                automationResults.push(autoActionForUser)
-            }
-        }
-
-        for (const result of automationResults) {
-            try {
-                await logAutomationResult(result)
-            } catch (err) {
-                jobError = 'Error occured when adding log entry: ' + err?.toString()
-                logger.error(err?.toString())
-            }
-        }
-    } catch (err) {
-        jobError = 'Error occured: ' + err?.toString()
-        logger.error(err?.toString())
+      const config = { user, eracuniConfig };
+      // TODO: if exception is thrown inside 'handleAutomationForUser' it will not be logged ?
+      const autoActionForUser = await handleAutomationForUser(config, curTime);
+      if (autoActionForUser === null) {
+        logger.info(`User ${user.username}. Nothing to do...`);
+      } else {
+        automationResults.push(autoActionForUser);
+      }
     }
 
-    if (parentPort) {
-        parentPort.postMessage(jobError ?? 'done');
+    for (const result of automationResults) {
+      try {
+        await logAutomationResult(result);
+      } catch (err) {
+        jobError = "Error occured when adding log entry: " + err?.toString();
+        logger.error(err?.toString());
+      }
     }
+  } catch (err) {
+    jobError = "Error occured: " + err?.toString();
+    logger.error(err?.toString());
+  }
 
-    logger.info('end')
-    logger.end()
-    process.exit(jobError ? 1 : 0)
+  if (parentPort) {
+    parentPort.postMessage(jobError ?? "done");
+  }
+
+  logger.info("end");
+  logger.end();
+  process.exit(jobError ? 1 : 0);
 })();
