0a3983393ab49b1e6da793ece06055d1ef452192
hktari <bkamnik1995@gmail.com>
2024-01-28 07:08:00 +0100

    require logger in main.js

--- browser-automation/main.js
+++ browser-automation/main.js
@@ -1,26 +1,24 @@
-
-const Bree = require('bree');
-const Graceful = require('@ladjs/graceful');
+const Bree = require("bree");
+const logger = require("./util/logging");
+const Graceful = require("@ladjs/graceful");
 
 let assistantJob = {
-    name: 'browser-automation-job',
-    // run on start as well
-    timeout: 0
-}
+  name: "browser-automation-job",
+  // run on start as well
+  timeout: 0,
+};
 
-console.log('node environment: ' + process.env.NODE_ENV)
+logger.info("node environment: " + process.env.NODE_ENV);
 
-if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
-    require('dotenv').config()
-    assistantJob.interval = '10s';
+if (process.env.NODE_ENV === "development" || process.env.NODE_ENV === "test") {
+  require("dotenv").config();
+  assistantJob.interval = "10s";
 } else {
-    assistantJob.cron = '*/5 * * * *';
+  assistantJob.cron = "*/5 * * * *";
 }
 
 const bree = new Bree({
-    jobs: [
-        assistantJob
-    ]
+  jobs: [assistantJob],
 });
 
 // handle graceful reloads, pm2 support, and events like SIGHUP, SIGINT, etc.
@@ -29,5 +27,5 @@ graceful.listen();
 
 // start all jobs (this is the equivalent of reloading a crontab):
 (async () => {
-    await bree.start();
+  await bree.start();
 })();
